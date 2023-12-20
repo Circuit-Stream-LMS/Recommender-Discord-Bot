8426f2e58d977e1ca347579c3e6ebc91a15b846e
Feromond <Jacobpmish@gmail.com>
2023-12-20 14:16:58 -0700

    Allowed for generic movie names to be used instead of ids

--- cogs/recommend.py
+++ cogs/recommend.py
@@ -5,75 +5,50 @@ Description:
 
 Version: 6.1.0
 """
-
-
 import pandas as pd
 from surprise import Dataset, Reader, SVD
 from surprise.model_selection import train_test_split
-from surprise import accuracy
 from discord.ext import commands
 from discord.ext.commands import Context
+from fuzzywuzzy import process
 
-
-# Here we name the cog and create a new class for the cog.
 class Recommend(commands.Cog, name="recommend"):
     def __init__(self, bot) -> None:
         self.bot = bot
-
-        # Load the MovieLens dataset (you can replace this with your dataset)
         self.data = Dataset.load_builtin('ml-100k')
         self.algo = SVD()
-
-        # Split the data into a training set and a test set
         self.trainset, self.testset = train_test_split(self.data, test_size=0.25)
-
-        # Train the algorithm on the training set
         self.algo.fit(self.trainset)
+        self.movie_titles = self.load_movie_titles()
+
+    def load_movie_titles(self):
+        item_file = 'ml-100k/u.item'
+        item_data = pd.read_csv(item_file, delimiter='|', encoding='ISO-8859-1', 
+                                usecols=[0, 1], names=['movie_id', 'title'])
+        return dict(zip(item_data['title'], item_data['movie_id']))
 
     @commands.hybrid_command(
         name="recommend",
-        description="Get recommendations based on user and item IDs.",
+        description="Get recommendations based on user ID and partial movie name.",
     )
-    async def recommend(self, context: Context, user_id: int, item_id: int) -> None:
-        """
-        This command provides recommendations based on user and item IDs.
-
-        :param context: The application command context.
-        :param user_id: The user ID for which recommendations are needed.
-        :param item_id: The item ID to base the recommendations on.
-        """
+    async def recommend(self, context: Context, user_id: int, partial_movie_name: str) -> None:
         try:
-            # Make a prediction using the provided user_id and item_id
-            prediction = self.algo.predict(str(user_id), str(item_id))
+            # Find the best match for the partial movie name
+            closest_match = process.extractOne(partial_movie_name, self.movie_titles.keys(), score_cutoff=70)
+            if not closest_match:
+                await context.send("No close match found for the movie name. Please try again.")
+                return
 
-            # Send the recommendation to Discord
-            await context.send(f"Prediction for User {user_id} on Item {item_id}:")
+            movie_name, movie_id = closest_match[0], self.movie_titles[closest_match[0]]
+            prediction = self.algo.predict(str(user_id), str(movie_id))
+            await context.send(f"Closest match: '{movie_name}'")
+            await context.send(f"Prediction for User {user_id} on Movie '{movie_name}':")
             await context.send(f"Rating Prediction: {prediction.est}")
 
         except Exception as e:
             await context.send(f"An error occurred: {str(e)}")
 
+    # Rest of your class and error handling remains the same
 
-
-    @recommend.error
-    async def recommend_error(self, context: Context, error) -> None:
-        """
-        Error handling for the 'recommend' command.
-
-        :param context: The application command context.
-        :param error: The error that was raised.
-        """
-        if isinstance(error, commands.BadArgument):
-            await context.send("Invalid input. Please make sure user ID and item ID are numbers.")
-        elif isinstance(error, commands.MissingRequiredArgument):
-            await context.send("Missing arguments. Please provide both a user ID and an item ID.")
-        else:
-            # Handle other types of errors here
-            await context.send("An error occurred. Please try again.")
-
-
-
-# And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
 async def setup(bot) -> None:
     await bot.add_cog(Recommend(bot))
-
